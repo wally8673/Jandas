2c098e2e0b583892c1487dce302e7290c176ac86
wally8673 <wmvillalba@estudiantes.unsam.edu.ar>
2025-06-03 15:51:02 -0300

    agregando nueva funcionalidad

--- src/jandas/Dataframe.java
+++ src/jandas/Dataframe.java
@@ -1,5 +1,32 @@
 package jandas;
 
-public class Dataframe {
-    
+import java.util.List;
+
+public class Dataframe{
+    private List<Columna<?>> columnas;
+    private List<Etiqueta<?>> etiquetasFilas;
+
+
+    public Dataframe(List<Columna<?>> columnas, List<Etiqueta<?>> etiquetasFilas){
+        this.columnas = columnas;
+        this.etiquetasFilas = etiquetasFilas;
+    }
+
+    public int cantColumnas(){
+        return columnas.size();
+    }
+    public int cantFilas(){
+        return columnas.get(0).getCeldas().size();
+    }
+    public void etiquetasColum(){
+        for(Columna<?> col: columnas){
+            System.out.println(col.getNombre().getValor());
+        }
+    }
+    public void etiquetasFila(){
+        for(Etiqueta<?> etq : etiquetasFilas){
+            System.out.println(etq.getValor());
+        }
+    }
+    public
 }
