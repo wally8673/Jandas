7fb8cce3a1ce9be07f245211b7908ae2e597152e
AgustinRebechi <rebechi.agustin@gmail.com>
2025-06-03 16:44:04 -0300

    constructor

--- src/jandas/Dataframe.java
+++ src/jandas/Dataframe.java
@@ -1,9 +1,38 @@
 package jandas;
 
+import jandas.Etiquetas.Etiqueta;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class Dataframe {
 
     private List<Columna<?>> columnas;
+    private List<Etiqueta> etiquetasFilas;
+    private List<Etiqueta> etiquetaColumnas;
+
+
+    public Dataframe(List<Columna<?>> columnas, List<Etiqueta> etiquetasFilas){
+        this.columnas = new ArrayList<>();
+        this.etiquetasFilas = new ArrayList<>();
+        this.etiquetaColumnas = new ArrayList<>();
+    }
+
+    public int cantColumnas(){
+        return columnas.size();
+    }
+    public int cantFilas(){
+        return columnas.get(0).getCeldas().size();
+    }
+    public void etiquetasColum(){
+        for(Columna<?> col: columnas){
+            System.out.println(col.getNombre().getValor());
+        }
+    }
+    public void etiquetasFila(){
+        for(Etiqueta<?> etq : etiquetasFilas){
+            System.out.println(etq.getValor());
+        }
+    }
     
 }
